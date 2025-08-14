22baefe3f61d89eb1f6d714679025659fd841469
Nabilleb <nabilgreen500@gmail.com>
2025-08-14 12:18:26 +0300

    Initialize task list variables and load tasks from localStorage

---
+++ index.js
@@ -0,0 +1,7 @@
+const taskList = document.getElementById('taskList');
+const addForm = document.getElementById("addForm");
+const taskInput = document.getElementById("taskInput");
+const errorMessage = document.getElementById("errorMessage");
+const filterButtons = document.querySelectorAll('.filter-btn');
+
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
